c2cee7a9860b021fbde8adfd3bccdf08549bd11d
Leo Lu <llu@lcs.on.ca>
2022-02-07 19:23:51 -0500

    Initial Commit

---
+++ ArthmeticAce/ArthmeticAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArthmeticAceApp.swift
+//  ArthmeticAce
+//
+//  Created by Leo Lu on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArthmeticAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
